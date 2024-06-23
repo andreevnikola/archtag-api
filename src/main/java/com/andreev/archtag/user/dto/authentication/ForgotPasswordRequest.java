5e61b535f0cc3bd21a52b29ddc468e9b3ce581bb
Vladimir Pasev <vladigpasev@gmail.com>
2024-06-24 01:30:59 +0300

    RESET PASSWORD WORKING

---
+++ src/main/java/com/andreev/archtag/user/dto/authentication/ForgotPasswordRequest.java
@@ -0,0 +1,19 @@
+package com.andreev.archtag.user.dto.authentication;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ForgotPasswordRequest {
+
+    @NotBlank
+    @Email
+    private String email;
+}
